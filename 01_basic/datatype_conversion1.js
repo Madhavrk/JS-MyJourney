f2c9b9e41bf44787cc87cb79c3984cf48d70aca3
Madhavrk <103875866+Madhavrk@users.noreply.github.com>
2025-12-18 12:06:20 +0000

    conversion of datatypes

--- 01_basic/datatype_conversion1.js
+++ 01_basic/datatype_conversion1.js
@@ -1,1 +1,29 @@
-let score = 33
+let score = true
+
+// console.log(typeof(score));
+
+let val = Number(score)
+// console.log(typeof(val));
+// console.log(val)
+
+/*
+"33" => String
+33 => number
+undefined => NaN
+null => 0
+true/false => 1/0
+*/
+
+let isLoggedIn = null
+
+let bool = Boolean(isLoggedIn)
+console.log(typeof bool)
+console.log(bool)
+
+/*
+true => boolean
+123 => true
+"abc" => true
+undefined => false
+null => false
+*/
